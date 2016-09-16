132a6b0149fe998021b3343cf06561b287aaf9f6
Yuri Reis <yuri.reis@msn.com>
2016-09-16 19:09:52 -0300

    attemps to Travis CI with Jekyll

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+et -e # halt script on error
+
+# config
+git config --global user.email "yurireis.rpg@gmail.com"
+git config --global user.name "Yuri Reis"
+git config --global push.default matching
+git config --global core.autocrlf false
+git config --global credential.helper store
+echo "https://${GithubPassword}:@github.com" > ~/.git-credentials
+
+# deploy
+if [[ `git status --porcelain` ]]; then
+    git stash
+    git checkout master
+    git stash apply
+    git add .
+    git commit -m 'Updated from travis [ci skip]'
+    git push origin master
+fi
+
+cd ./_site
+
+git init
+git add --all
+git commit -m "Deploy to GitHub Pages"
+git remote add origin ${GH_REMOTE}
+git push --force --quiet origin gh-pages
